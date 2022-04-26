7cb77cf360cdf6b5202cab48887ebd3ddd0eb46c
Flux State <f1ux.state@pm.me>
2022-04-26 14:31:04 +0200

    more random sleep to combat captcha, ip check

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 killall -9 python3
 
 killall openvpn
-sleep "$(shuf -i 5-10 -n 1)"
+sleep "$(shuf -i 1-10 -n 1)"
 
 set -e
 
@@ -13,6 +13,8 @@ cd /opt/pia/
 PIA_DNS='false' PIA_PF='false' VPN_PROTOCOL='openvpn_udp_standard' DISABLE_IPV6='yes' \
 PREFERRED_REGION="$(shuf -n 1 /config/regions | sed -e 's/\r//' | sed -e 's/\n//')" \
 /opt/pia/run_setup.sh
+
+echo -e "$(curl 'https://api.my-ip.io/ip' 2> /dev/null)\n"
 
 shuf /config/resolv.conf >/etc/resolv.conf
 
